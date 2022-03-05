16c2f7a8790236d9f7e88a3574f92923efe41b1a
Hwang <dudhkd98@naver.com>
2022-03-05 23:00:56 +0900

    [FEAT]: camera basic setting - issue: #2

---
+++ DEARPHOTO/DEARPHOTO/Screen/Camera/Controller/CameraVC.swift
@@ -0,0 +1,104 @@
+//
+//  CameraVC.swift
+//  DEARPHOTO
+//
+//  Created by 황윤경 on 2022/03/05.
+//
+
+import UIKit
+import AVFoundation
+import Photos
+
+class CameraVC: UIViewController {
+    @IBOutlet weak var captureImageView: UIImageView!
+    @IBOutlet weak var previewView: UIView!
+    
+    var captureSession: AVCaptureSession!
+    var stillImageOutput: AVCapturePhotoOutput!
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupCameraView()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.captureSession.stopRunning()
+    }
+    
+    @IBAction func didTakePhoto(_ sender: Any) {
+        let settings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        stillImageOutput.capturePhoto(with: settings, delegate: self)
+    }
+    
+    func setupCameraView() {
+        
+        captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .medium
+        
+        guard let backCamera = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: backCamera)
+            stillImageOutput = AVCapturePhotoOutput()
+            
+            if captureSession.canAddInput(input) && captureSession.canAddOutput(stillImageOutput) {
+                captureSession.addInput(input)
+                captureSession.addOutput(stillImageOutput)
+                setupLivePreview()
+            }
+        }
+        catch let error  {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func setupLivePreview() {
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+        videoPreviewLayer.connection?.videoOrientation = .portrait
+        previewView.layer.addSublayer(videoPreviewLayer)
+        
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            guard let self = self else { return }
+            self.captureSession.startRunning()
+            DispatchQueue.main.async {
+                self.videoPreviewLayer.frame = self.previewView.bounds
+            }
+        }
+    }
+    
+    func saveImage() {
+        PHPhotoLibrary.requestAuthorization { status in
+            guard status == .authorized else { return }
+            
+            DispatchQueue.main.async {
+                guard let image = self.captureImageView.image else {
+                    return
+                }
+                
+                PHPhotoLibrary.shared().performChanges({
+                    PHAssetChangeRequest.creationRequestForAsset(from: image)
+                }, completionHandler: nil)
+            }
+        }
+    }
+}
+
+extension CameraVC: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+        guard let imageData = photo.fileDataRepresentation()
+        else { return }
+        
+        let image = UIImage(data: imageData)
+        captureImageView.image = image
+        
+        DispatchQueue.main.async {
+            self.saveImage()
+        }
+    }
+}
